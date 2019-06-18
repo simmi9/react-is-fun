4708c0f10bbc4a1a3ed444a59915cffc537cd4d3
simmi9 <bhawna.keswani20@gmail.com>
2019-06-18 17:03:05 -0400

    created an element title and rendered it in root

--- src/index.js
+++ src/index.js
@@ -4,7 +4,14 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const title = React.createElement('h1',
+ 								{id:'title', 
+ 								className:'header'}, 
+ 								'Hello World'
+								)  
+
+ReactDOM.render(title, document.getElementById('root'));      
+//ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
